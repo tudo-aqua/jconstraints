5f2e329c8b95a16ae0376dbee17c5304ae79c211
Oxana Warkentin <oxana.warkentin@tu-dortmund.de>
2021-10-17 21:46:07 +0200

    license

--- jconstraints-core/src/main/java/gov/nasa/jpf/constraints/normalization/experimentalVisitors/ModifiedMiniScopingVisitor.java
+++ jconstraints-core/src/main/java/gov/nasa/jpf/constraints/normalization/experimentalVisitors/ModifiedMiniScopingVisitor.java
@@ -16,6 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package gov.nasa.jpf.constraints.normalization.experimentalVisitors;
 
 import gov.nasa.jpf.constraints.api.Expression;
--- jconstraints-core/src/test/java/gov/nasa/jpf/constraints/normalization/ConjunctionCreatorTest.java
+++ jconstraints-core/src/test/java/gov/nasa/jpf/constraints/normalization/ConjunctionCreatorTest.java
@@ -16,6 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package gov.nasa.jpf.constraints.normalization;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
