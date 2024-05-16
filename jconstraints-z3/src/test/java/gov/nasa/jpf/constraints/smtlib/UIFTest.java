b940b36c72396441eca2c3f8a7c5de7fc10a4c97
Falk Howar <falk.howar@gmail.com>
2024-05-16 22:11:42 +0200

    add unsat test

--- jconstraints-z3/src/test/java/gov/nasa/jpf/constraints/smtlib/UIFTest.java
+++ jconstraints-z3/src/test/java/gov/nasa/jpf/constraints/smtlib/UIFTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UIFTest {
     @Test
-    public void Problem2Test() throws IOException, SMTLIBParserException, URISyntaxException {
+    public void Problem1Test() throws IOException, SMTLIBParserException, URISyntaxException {
         URL smtFile = QfLiaTest.class.getClassLoader().getResource("problem_2__008.smt2");
         SMTProblem problem =
                 SMTLIBParser.parseSMTProgram(
@@ -30,5 +30,20 @@ public class UIFTest {
         Valuation model = new Valuation();
         ConstraintSolver.Result jRes = z3.solve(problem.getAllAssertionsAsConjunction(), model);
         assertEquals(ConstraintSolver.Result.SAT, jRes);
+    }
+    @Test
+    public void Problem2Test() throws IOException, SMTLIBParserException, URISyntaxException {
+        URL smtFile = QfLiaTest.class.getClassLoader().getResource("problem_2__008.smt2");
+        SMTProblem problem =
+                SMTLIBParser.parseSMTProgram(
+                        "(declare-fun extends (String String) Bool)" +
+                                "(assert (extends \"a\" \"b\"))" +
+                                "(assert (not (extends \"a\" \"b\")))"
+                );
+
+        NativeZ3Solver z3 = new NativeZ3Solver();
+        Valuation model = new Valuation();
+        ConstraintSolver.Result jRes = z3.solve(problem.getAllAssertionsAsConjunction(), model);
+        assertEquals(ConstraintSolver.Result.UNSAT, jRes);
     }
 }
