2401e6afb85be242b147e733e9a991da87748758
Malte Mues <mail.mues@gmail.com>
2025-11-13 20:02:44 +0100

    Make linux the default build target, needs improvement

--- jconstraints-cvc5/build.gradle.kts
+++ jconstraints-cvc5/build.gradle.kts
@@ -35,6 +35,7 @@ repositories{
 dependencies {
     implementation("org.apache.commons:commons-math3:3.6.1")
     implementation(project(":jconstraints-core"))
-    implementation(files("libs/cvc5-macOs-arm64-java-api.jar"))
+    //implementation(files("libs/cvc5-macOs-arm64-java-api.jar"))
+    implementation(files("libs/cvc5-Linux-x86_64-java-api.jar"))
     //testImplementation(project(":jconstraints-benchmarktest")) //FIXME: We need to change this so that it is not executed on windows.
 }
